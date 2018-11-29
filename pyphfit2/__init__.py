c95c479a05212a040bff8596a68d168150185dc2
findesgh <findessp@yandex.ru>
2018-11-29 16:31:22 +0100

    First version of setup.py

---
+++ pyphfit2/__init__.py
@@ -0,0 +1,2 @@
+from pyphfit2.phfit2 import phfit2
+__all__ = ['phfit2']
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name='pyphfit2',
+    version='0.1.dev',
+    author_email='findessp@yandex.ru',
+    description='Python wrapper for the Verner et al. phfit2.',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/findesgh/pyphfit2',
+    license='GPL-3.0',
+    packages=['pyphfit2'],
+    install_requires=[
+        'numpy',
+        'astropy',
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    data_files=[('pyphfit2/lib', ['pyphfit2/lib/phfit2.so'])]
+)
